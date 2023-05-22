f8b73cd694cb8b1326548d6ac2ee10662e0e96a8
Bas-Tobback <bas.tobback@student.uantwerpen.be>
2023-05-22 16:36:34 +0200

    all done, it works, dont touch it you fool

--- DS2_files/songs_persistence/init.sh
+++ DS2_files/songs_persistence/init.sh
@@ -11,4 +11,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "songs" <<-EOSQL
         title TEXT NOT NULL,
         PRIMARY KEY (artist, title)
     );
+    COPY songs (artist, title)
+    FROM '/docker-entrypoint-initdb.d/mil_song.csv'
+    DELIMITER ','
+    CSV HEADER;
 EOSQL
